aac3f020a5679e2ae5f51eff3e4acf3d33ef78a8
kabinhamal <80996252+kabinhamal@users.noreply.github.com>
2023-04-16 18:58:05 -0400

    Update variables.tf

--- iac/variables.tf
+++ iac/variables.tf
@@ -2,18 +2,18 @@
 variable "azure_subscription_id" {
   type        = string
   description = "The Subscription ID which should be used"
-  default = "17983203-31aa-4998-a994-16bcb84f3dff"
+  default = "123"
 }
 variable "azure_client_id" {
   type        = string
   description = "The Client ID which should be used."
-  default = "6849b684-2d1e-415b-ac28-44d9e6f9e35b" 
+  default = "68" 
 }
 
 variable "azure_tenant_id" {
   type        = string
   description = "The Tenant ID should be used."
-  default = "f946600a-6e91-4c31-a96f-95977fd26317"
+  default = "f94"
 }
 
 # variable "owner_id" {
@@ -26,7 +26,7 @@ variable "azure_tenant_id" {
 variable "resource_group_name" {
   type        = string
   description = "(Optional) Name of resource group to create. Defaults to oidc-test."
-  default     = "oidc-test-rsgroup"
+  default     = "oidc-test-rgp"
 }
 
 variable "location" {
