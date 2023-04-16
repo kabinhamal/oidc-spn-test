0e9d14df60a9e1dd6aae4296e90ebfabdcbc7263
kabinhamal <80996252+kabinhamal@users.noreply.github.com>
2023-04-16 19:26:11 -0400

    Update provider.tf

--- iac/provider.tf
+++ iac/provider.tf
@@ -30,7 +30,6 @@ provider "azurerm" {
 # Azure AD Provider
 provider "azuread" {
   client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
   tenant_id       = var.azure_tenant_id
   use_oidc         = true
 }
