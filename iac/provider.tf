5d14d1f5bc9781aa7fcc78ff5c324cffa184b0b7
kabinhamal <80996252+kabinhamal@users.noreply.github.com>
2023-04-16 16:33:32 -0400

    Update provider.tf

--- iac/provider.tf
+++ iac/provider.tf
@@ -27,8 +27,6 @@ provider "azurerm" {
   tenant_id         = var.azure_tenant_id
   subscription_id   = var.azure_subscription_id
   client_id         = var.azure_client_id
-  client_secret     = var.azure_client_secret
-
   features {}
   use_oidc = true
 }
