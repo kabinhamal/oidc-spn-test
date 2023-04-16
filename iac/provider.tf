956f97df10906afa2bc36439179f948181b87f6c
kabinhamal <80996252+kabinhamal@users.noreply.github.com>
2023-04-16 16:35:04 -0400

    Update provider.tf

--- iac/provider.tf
+++ iac/provider.tf
@@ -36,6 +36,7 @@ provider "azuread" {
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
   tenant_id       = var.azure_tenant_id
+  use_oidc         = true
 }
 
 
