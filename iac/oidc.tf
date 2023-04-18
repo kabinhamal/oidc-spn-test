58d130accdc67527d93013502355d16a4fb2688f
kabinhamal <80996252+kabinhamal@users.noreply.github.com>
2023-04-18 14:16:55 -0400

    Update oidc.tf

--- iac/oidc.tf
+++ iac/oidc.tf
@@ -1,6 +1,6 @@
-data "environment_variables" "all" {}
+# data "environment_variables" "all" {}
 
-resource "local_file" "workspace_token" {
-  content  = data.environment_variables.all.items["TFC_WORKLOAD_IDENTITY_TOKEN"]
-  filename = "~/.azure/token"
-}
+# resource "local_file" "workspace_token" {
+#   content  = data.environment_variables.all.items["TFC_WORKLOAD_IDENTITY_TOKEN"]
+#   filename = "~/.azure/token"
+# }
