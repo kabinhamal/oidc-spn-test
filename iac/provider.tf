a6feb71b86e093f8b7eccf2cad6d40fda5b64e06
kabinhamal <80996252+kabinhamal@users.noreply.github.com>
2023-04-18 20:18:18 -0400

    Update provider.tf

--- iac/provider.tf
+++ iac/provider.tf
@@ -10,13 +10,13 @@ terraform {
       version = "~>3.33.0"
       source  = "hashicorp/azurerm"    
     }
-#       source  = "EppO/environment"
-#       version = "~>1.0"
-#     }
-#     local = {
-#       source  = "hashicorp/local"
-#       version = "~>2.0"
-#     }
+      source  = "EppO/environment"
+      version = "~>1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~>2.0"
+    }
   }
   }
 
@@ -35,20 +35,20 @@ terraform {
 
 
 provider "azurerm" {
-  tenant_id         = var.azure_tenant_id
-  subscription_id   = var.azure_subscription_id
-  client_id         = var.azure_client_id
+#   tenant_id         = var.azure_tenant_id
+#   subscription_id   = var.azure_subscription_id
+#   client_id         = var.azure_client_id
   features {}
   use_oidc = true
   #oidc_token = data.environment_variables.all.items["TFC_WORKLOAD_IDENTITY_TOKEN"]
 }
 
-# Azure AD Provider
-provider "azuread" {
-  client_id       = var.azure_client_id
-  tenant_id       = var.azure_tenant_id
-  use_oidc         = true
-}
+# # Azure AD Provider
+# provider "azuread" {
+#   client_id       = var.azure_client_id
+#   tenant_id       = var.azure_tenant_id
+#   use_oidc         = true
+# }
 
 
 
